f235937ec105061787c1d217946096f8c35c2deb
Ebn Zhang <ebnbin@gmail.com>
2018-02-03 23:09:54 +0800

    Create app icons and themes

--- app/src/main/java/com/ebnbin/floatingcamera/MainActivity.kt
+++ app/src/main/java/com/ebnbin/floatingcamera/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.ebnbin.floatingcamera
 
-import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTaskDescription(taskDescription)
+        setTheme(R.style.LightTheme)
+
         super.onCreate(savedInstanceState)
 
         val button = Button(this)
--- app/src/main/java/com/ebnbin/floatingcamera/Utils.kt
+++ app/src/main/java/com/ebnbin/floatingcamera/Utils.kt
@@ -1,6 +1,11 @@
 package com.ebnbin.floatingcamera
 
+import android.app.ActivityManager
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.support.annotation.StringRes
+import android.support.v4.util.ArrayMap
 import android.view.WindowManager
 
 //*********************************************************************************************************************
@@ -18,3 +23,36 @@ val app by lazy { AppApplication.instance }
 // System services.
 
 val windowManager by lazy { app.getSystemService(Context.WINDOW_SERVICE) as WindowManager }
+
+//*********************************************************************************************************************
+// Resources.
+
+val resources by lazy { app.resources!! }
+
+private val stringCache by lazy { ArrayMap<@StringRes Int, String>() }
+
+fun getString(@StringRes stringRes: Int) = stringCache.getOrPut(stringRes) { resources.getString(stringRes) }!!
+
+val Int.dp get() = this * app.resources.displayMetrics.density
+val Int.dpInt get() = dp.toInt()
+
+//*********************************************************************************************************************
+// TaskDescription.
+
+@Suppress("DEPRECATION")
+val taskDescription by lazy {
+    val label = getString(R.string.app_name)
+
+    val size = 48.dpInt
+    val icon = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(icon)
+    val drawable = resources.getDrawable(R.drawable.logo)
+    val tintColor = resources.getColor(R.color.dark_icon)
+    drawable.setTint(tintColor)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+
+    val colorPrimary = resources.getColor(R.color.light_color_primary)
+
+    ActivityManager.TaskDescription(label, icon, colorPrimary)
+}
