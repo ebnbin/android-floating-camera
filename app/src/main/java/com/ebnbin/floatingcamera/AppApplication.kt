e6dde2177877e8ace644795c5e0cce61576c4204
Ebn Zhang <ebnbin@gmail.com>
2018-04-26 20:59:06 +0800

    Add Firebase and Fabric

--- app/src/main/java/com/ebnbin/floatingcamera/AppApplication.kt
+++ app/src/main/java/com/ebnbin/floatingcamera/AppApplication.kt
@@ -1,8 +1,10 @@
 package com.ebnbin.floatingcamera
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.ebnbin.floatingcamera.util.BaseRuntimeException
 import com.ebnbin.floatingcamera.util.PreferenceHelper
+import io.fabric.sdk.android.Fabric
 
 /**
  * 应用 [Application] 类.
@@ -12,6 +14,8 @@ class AppApplication : Application() {
         super.onCreate()
 
         singleton = this
+
+        Fabric.with(this, Crashlytics())
 
         PreferenceHelper
     }
